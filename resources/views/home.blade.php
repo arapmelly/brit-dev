77942b1459c0312277729d60f54a440d0e5b6f71
Jacob Chumo <jacob.chumo@dentsu.com>
2023-03-24 12:29:46 +0300

    installed collapse field type

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,21 +15,19 @@
 
 @foreach ($pages as $page)
 
-
 @if(count($page->sections) > 0) 
 
     @foreach($page->sections as $section)
 
         @if($section->enabled)
-            
+        
             @include('partials.'.$section->section_name->slug)
+
         @endif
 
     @endforeach
 @endif
-
-   
+ 
 @endforeach
-
 
 @endsection
